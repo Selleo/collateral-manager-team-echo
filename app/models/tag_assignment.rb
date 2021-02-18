fa67132a7d4ae93f3399b7aae93cdfd8cf56d3fc
wibor <wborochow@gmail.com>
2021-02-18 19:11:22 +0100

    feat: add weight to tag assignment

--- app/models/tag_assignment.rb
+++ app/models/tag_assignment.rb
@@ -2,7 +2,7 @@ class TagAssignment < ApplicationRecord
   belongs_to :taggable, polymorphic: true
   belongs_to :tag
 
-  def self.assign(tag, resource)
-    TagAssignment.create(tag: tag, taggable_id: resource.id, taggable_type: resource.class.to_s)
+  def self.assign(tag, resource, weight)
+    TagAssignment.create(tag: tag, taggable_id: resource.id, taggable_type: resource.class.to_s, weight:weight)
   end
 end
